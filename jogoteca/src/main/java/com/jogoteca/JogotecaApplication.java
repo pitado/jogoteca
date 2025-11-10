3c0c7511c2a4cdbe4c85741489e7bbc7ed949446
Miguel Pita <Miguellpitaa@gmail.com>
2025-11-10 17:29:13 -0300

    Update JogotecaApplication.java

--- jogoteca/src/main/java/com/jogoteca/JogotecaApplication.java
+++ jogoteca/src/main/java/com/jogoteca/JogotecaApplication.java
@@ -1,9 +1,11 @@
 package com.jogoteca;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
 public class JogotecaApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(JogotecaApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(JogotecaApplication.class, args);
+  }
 }
